4044d04d4c39d1cb816b22c342cf60e477ebcfe2
joebonjovi <64037041+joebonjovi@users.noreply.github.com>
2020-04-21 12:45:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function modal(){
+if(document.getElementById('name', 'email', 'phone', 'pageviews', 'promote', 'choice', 'url').value != ""){
+alert('Thank you for taking the time to apply!');   
+}
+}
